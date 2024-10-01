c97642e660b0bc9571a3aea41c959204ff2f0734
borissic2 <sicbj01@gmail.com>
2024-09-30 22:27:34 -0600

    das

---
+++ week02/scripts/bom.js
@@ -0,0 +1,32 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('------');
+        
+
+button.addEventListener('click', function(){
+    if (input.value.trim() !=='') {
+        const li = document.createElement('li');
+        
+        const deleteButton = document.createElement('button');
+        
+        li.textContent = input.value;
+        
+        deleteButton.textContent = '❌';
+        
+        li.append(deleteButton);
+        
+        li.append(li);
+
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    
+    } else {
+        input.focus();
+    }   
+});
+
