96864d583afd97511cc251e3748c41dd270002fe
borissic2 <sicbj01@gmail.com>
2024-10-03 23:09:50 -0600

    da

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modification: ${lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
